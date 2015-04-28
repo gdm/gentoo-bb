beb4f9178b6e2f2382e5524c3fac44d39c80eaa9
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2015-04-28 03:13:36 +0200

    Add working ui example for docker

---
+++ todo/dockerUItest/test.sh
@@ -0,0 +1,11 @@
+docker build -t xeyes - << __EOF__
+FROM debian
+RUN apt-get update
+RUN apt-get install -qqy x11-apps
+ENV DISPLAY :0
+CMD xeyes
+__EOF__
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH xeyes
