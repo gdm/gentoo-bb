f6dd8a924cc416b0b74d7032fb7da134fb269695
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2015-05-19 03:00:36 +0200

    Added exim with dovecot

---
+++ dock/gentoobb/images/exim-dovecot/Buildconfig.sh
@@ -0,0 +1,28 @@
+#
+# build config
+#
+PACKAGES="net-misc/curl mail-mta/exim net-mail/dovecot app-shells/bash"
+
+#
+# this method runs in the bb builder container just before building the rootfs
+#
+configure_rootfs_build()
+{
+    update_use 'mail-mta/exim' '+dovecot-sasl' '-ldap' '+maildir' '+sasl' '+sqlite'
+    update_use 'net-mail/dovecot' '+imapc' '-ldap' '+maildir' '+mbox' '+sqlite'
+    # reinstall curl, need at build time
+    unprovide_package net-misc/curl
+}
+
+#
+# this method runs in the bb builder container just before packing the rootfs
+#
+finish_rootfs_build()
+{
+    copy_gcc_libs
+    copy_locale_data
+    #mkdir -p $EMERGE_ROOT/var/run/mysql $EMERGE_ROOT/var/run/mysqld
+    #chown mysql:mysql $EMERGE_ROOT/var/run/mysql $EMERGE_ROOT/var/run/mysqld
+    # remove curl again
+    uninstall_package net-misc/curl
+}
