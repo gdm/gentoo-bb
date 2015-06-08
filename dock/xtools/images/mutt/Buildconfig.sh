6549d0629fc0e739c75094db8074ea8d2cfd581e
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2015-06-08 09:54:46 +0200

    An attempt to add mutt image

---
+++ dock/xtools/images/mutt/Buildconfig.sh
@@ -0,0 +1,33 @@
+#
+# build config
+#
+#PACKAGES="app-editors/vim app-shells/zsh sys-apps/shadow x11-wm/xpra"
+PACKAGES="app-editors/mutt"
+#KEEP_HEADERS=true
+
+#
+# this method runs in the bb builder container just before starting the build of the rootfs
+#
+configure_rootfs_build()
+{
+  #  update_use '+X'
+
+    #echo 'PYTHON_TARGETS="python2_7"' >> /etc/portage/make.conf
+    #echo 'PYTHON_SINGLE_TARGET="python2_7"' >> /etc/portage/make.conf
+    #echo 'USE_PYTHON="2.7"' >> /etc/portage/make.conf
+    #echo 'VIDEO_CARDS="dummy fbdev"' >> /etc/portage/make.conf
+   
+    #update_use 'x11-wm/xpra' '-client' '+server' '+x264'
+    #update_use 'dev-lang/python' '+sqlite'
+    #emerge -uDN --tree world
+}
+
+#
+# this method runs in the bb builder container just before tar'ing the rootfs
+#
+finish_rootfs_build()
+{
+  :
+    # required by fontconfig
+    #copy_gcc_libs
+}
---
+++ dock/xtools/images/mutt/README.md
@@ -0,0 +1,2 @@
+Run this mutt image with:
+
