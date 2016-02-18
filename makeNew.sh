073f8f1d73354073bb2be7f15751486f361997b0
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2016-02-19 00:07:28 +0100

    Update min mysql client

--- makeNew.sh
+++ makeNew.sh
@@ -13,13 +13,13 @@ cat <<END > dock/${MY_NS}/images/${IMG_NAME}/Buildconfig.sh
 PACKAGES="dev-db/mysql"
 configure_rootfs_build() 
 {
-    update_use 'dev-db/mysql' 'minimal -perl'
+    update_use 'dev-db/mysql' '+minimal -server -perl'
+    #unprovide_package dev-lang/perl sys-libs/ncurses-5.9-r99 sys-libs/db sys-apps/texinfo
 }
 
 finish_rootfs_build()
 {
-    # remove curl again
-    uninstall_package net-misc/curl
+    uninstall_package dev-lang/perl =sys-libs/ncurses-5.9-r99 sys-libs/db app-arch/bzip2 app-portage/portage-utils app-shells/bash app-admin/perl-cleaner dev-perl/libintl-perl dev-libs/libaio app-admin/perl-cleaner 
 }
 END
 
