c82a7ee2dedb2126d14874d43a3ce29026268857
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2016-03-07 01:56:51 +0100

    Update building oracle jre

--- dock/gentoobb/images/jre-oracle/Buildconfig.sh
+++ dock/gentoobb/images/jre-oracle/Buildconfig.sh
@@ -11,15 +11,15 @@ configure_rootfs_build()
     update_use 'dev-java/oracle-jre-bin' '+jce'
 
     # download oracle jre bin
-    JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jre-8u72-linux-x64.tar.gz
-    #JRE_TAR=$(emerge -pf oracle-jre-bin 2>&1 >/dev/null | grep -m1 "jre-[0-9a-z]*-linux-x64\.tar\.gz")
+    JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jre-8u74-linux-x64.tar.gz
     regex="(jre-[0-9a-z]*-linux-x64\.tar\.gz)"
 
     [[ ${JRE_URL} =~ $regex ]] && JRE_TAR="${BASH_REMATCH[1]}"
     [ -n ${JRE_TAR} ] && [ ! -f /distfiles/${JRE_TAR} ] && download_from_oracle "${JRE_URL}"
 
-    JCE_URL=http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
-    [ ! -f /distfiles/${POLICY_URL} ] && download_from_oracle "${JCE_URL}"
+    JCE_FILE=jce_policy-8.zip
+    JCE_URL=http://download.oracle.com/otn-pub/java/jce/8/${JCE_FILE}
+    [ ! -f /distfiles/${JCE_FILE} ] && download_from_oracle "${JCE_URL}"
 
     update_use 'dev-java/oracle-jre-bin' '+headless-awt +jce -fontconfig'
     # skip python and iced-tea
